a430a0823ea7d6514df19d49b35f9f3ebbe183de
Jelena Gavrilovic <gavrilovicjelena@outlook.com>
2019-03-05 12:34:09 +0100

    merging with master

--- app/javascript/packs/quiz.js
+++ app/javascript/packs/quiz.js
@@ -1,76 +1,309 @@
-import * as Survey from "survey-jquery";
-Survey.Survey.cssType = "bootstrap";
+jQuery('.mm-prev-btn').hide();
 
-var json = {
-    title: "American History",
-    showProgressBar: "bottom",
-    showTimerPanel: "top",
-    maxTimeToFinishPage: 10,
-    maxTimeToFinish: 25,
-    firstPageIsStarted: true,
-    startSurveyText: "Start Quiz",
-    pages: [
-        {
-            questions: [
-                {
-                    type: "html",
-                    html: "You are about to start quiz by history. <br/>You have 10 seconds for every page and 25 seconds for the whole survey of 3 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "civilwar",
-                    title: "When was the Civil War?",
-                    choices: [
-                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
-                    ],
-                    correctAnswer: "1850-1900"
-                }
-            ]
-        }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "libertyordeath",
-                    title: "Who said 'Give me liberty or give me death?'",
-                    choicesOrder: "random",
-                    choices: [
-                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
-                    ],
-                    correctAnswer: "Patrick Henry"
-                }
-            ]
-        }, {
-            maxTimeToFinish: 15,
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "magnacarta",
-                    title: "What is the Magna Carta?",
-                    choicesOrder: "random",
-                    choices: [
-                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
-                    ],
-                    correctAnswer: "The foundation of the British parliamentary system"
-                }
-            ]
+  var x;
+  var count;
+  var current;
+  var percent;
+  var z = [];
+
+  init();
+  getCurrentSlide();
+  goToNext();
+  goToPrev();
+  getCount();
+  // checkStatus();
+  // buttonConfig();
+  buildStatus();
+  deliverStatus();
+  submitData();
+  goBack();
+
+  function init() {
+
+    jQuery('.mm-survey-container .mm-survey-page').each(function() {
+
+      var item;
+      var page;
+
+      item = jQuery(this);
+      page = item.data('page');
+
+      item.addClass('mm-page-'+page);
+      //item.html(page);
+
+    });
+
+  }
+
+  function getCount() {
+
+    count = jQuery('.mm-survey-page').length;
+    return count;
+
+  }
+
+  function goToNext() {
+
+    jQuery('.mm-next-btn').on('click', function() {
+      goToSlide(x);
+      getCount();
+      current = x + 1;
+      var g = current/count;
+      buildProgress(g);
+      var y = (count + 1);
+      getButtons();
+      jQuery('.mm-survey-page').removeClass('active');
+      jQuery('.mm-page-'+current).addClass('active');
+      getCurrentSlide();
+      checkStatus();
+      if( jQuery('.mm-page-'+count).hasClass('active') ){
+        if( jQuery('.mm-page-'+count).hasClass('pass') ) {
+          jQuery('.mm-finish-btn').addClass('active');
         }
-    ],
-    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
-};
+        else {
+          jQuery('.mm-page-'+count+' .mm-survery-content .mm-survey-item').on('click', function() {
+            jQuery('.mm-finish-btn').addClass('active');
+          });
+        }
+      }
+      else {
+        jQuery('.mm-finish-btn').removeClass('active');
+        if( jQuery('.mm-page-'+current).hasClass('pass') ) {
+          jQuery('.mm-survey-container').addClass('good');
+          jQuery('.mm-survey').addClass('okay');
+        }
+        else {
+          jQuery('.mm-survey-container').removeClass('good');
+          jQuery('.mm-survey').removeClass('okay');
+        }
+      }
+      buttonConfig();
+    });
 
-var survey = new Survey.Model(json);
+  }
 
-survey
-  .onComplete
-  .add(function(result) {
-    document
-      .querySelector('#surveyResult')
-      .innerHTML = "result: " + JSON.stringify(result.data);
+  function goToPrev() {
+
+    jQuery('.mm-prev-btn').on('click', function() {
+      goToSlide(x);
+      getCount();
+      current = (x - 1);
+      var g = current/count;
+      buildProgress(g);
+      var y = count;
+      getButtons();
+      jQuery('.mm-survey-page').removeClass('active');
+      jQuery('.mm-page-'+current).addClass('active');
+      getCurrentSlide();
+      checkStatus();
+      jQuery('.mm-finish-btn').removeClass('active');
+      if( jQuery('.mm-page-'+current).hasClass('pass') ) {
+        jQuery('.mm-survey-container').addClass('good');
+        jQuery('.mm-survey').addClass('okay');
+      }
+      else {
+        jQuery('.mm-survey-container').removeClass('good');
+        jQuery('.mm-survey').removeClass('okay');
+      }
+      buttonConfig();
+    });
+
+  }
+
+  function buildProgress(g) {
+
+    if(g > 1){
+      g = g - 1;
+    }
+    else if (g === 0) {
+      g = 1;
+    }
+    g = g * 100;
+    jQuery('.mm-survey-progress-bar').css({ 'width' : g+'%' });
+
+  }
+
+  function goToSlide(x) {
+
+    return x;
+
+  }
+
+  function getCurrentSlide() {
+
+    jQuery('.mm-survey-page').each(function() {
+
+      var item;
+
+      item = jQuery(this);
+
+      if( jQuery(item).hasClass('active') ) {
+        x = item.data('page');
+      }
+      else {
+
+      }
+
+      return x;
+
+    });
+
+  }
+
+  function getButtons() {
+
+    if(current === 0) {
+      current = y;
+    }
+    if(current === count) {
+      jQuery('.mm-next-btn').hide();
+    }
+    else if(current === 1) {
+      jQuery('.mm-prev-btn').hide();
+    }
+    else {
+      jQuery('.mm-next-btn').show();
+      jQuery('.mm-prev-btn').show();
+    }
+
+  }
+
+  jQuery('.mm-survey-q li input').each(function() {
+
+    var item;
+    item = jQuery(this);
+
+    jQuery(item).on('click', function() {
+      if( jQuery('input:checked').length > 0 ) {
+          // console.log(item.val());
+          jQuery('label').parent().removeClass('active');
+          item.closest( 'li' ).addClass('active');
+      }
+      else {
+        //
+      }
+    });
+
   });
 
-$("#surveyElement").Survey({
-  model: survey
-});
+  percent = (x/count) * 100;
+  jQuery('.mm-survey-progress-bar').css({ 'width' : percent+'%' });
+
+  function checkStatus() {
+    jQuery('.mm-survery-content .mm-survey-item').on('click', function() {
+      var item;
+      item = jQuery(this);
+      item.closest('.mm-survey-page').addClass('pass');
+    });
+  }
+
+  function buildStatus() {
+    jQuery('.mm-survery-content .mm-survey-item').on('click', function() {
+      var item;
+      item = jQuery(this);
+      item.addClass('bingo');
+      item.closest('.mm-survey-page').addClass('pass');
+      jQuery('.mm-survey-container').addClass('good');
+    });
+  }
+
+  function deliverStatus() {
+    jQuery('.mm-survey-item').on('click', function() {
+      if( jQuery('.mm-survey-container').hasClass('good') ){
+        jQuery('.mm-survey').addClass('okay');
+      }
+      else {
+        jQuery('.mm-survey').removeClass('okay');
+      }
+      buttonConfig();
+    });
+  }
+
+  function lastPage() {
+    if( jQuery('.mm-next-btn').hasClass('cool') ) {
+      alert('cool');
+    }
+  }
+
+  function buttonConfig() {
+    if( jQuery('.mm-survey').hasClass('okay') ) {
+      jQuery('.mm-next-btn button').prop('disabled', false);
+    }
+    else {
+      jQuery('.mm-next-btn button').prop('disabled', true);
+    }
+  }
+
+  function submitData() {
+    jQuery('.mm-finish-btn').on('click', function() {
+      collectData();
+      jQuery('.mm-survey-bottom').slideUp();
+      jQuery('.mm-survey-results').slideDown();
+    });
+  }
+
+  function collectData() {
+
+    var map = {};
+    var ax = ['0','red','mercedes','3.14','3'];
+    var answer = '';
+    var total = 0;
+    var ttl = 0;
+    var g;
+    var c = 0;
+
+    jQuery('.mm-survey-item input:checked').each(function(index, val) {
+      var item;
+      var data;
+      var name;
+      var n;
+
+      item = jQuery(this);
+      data = item.val();
+      name = item.data('item');
+      n = parseInt(data);
+      total += n;
+
+      map[name] = data;
+
+    });
+
+    jQuery('.mm-survey-results-container .mm-survey-results-list').html('');
+
+    for (i = 1; i <= count; i++) {
+
+      var t = {};
+      var m = {};
+      answer += map[i] + '<br>';
+
+      if( map[i] === ax[i]) {
+        g = map[i];
+        p = 'correct';
+        c = 1;
+      }
+      else {
+        g = map[i];
+        p = 'incorrect';
+        c = 0;
+      }
+
+      jQuery('.mm-survey-results-list').append('<li class="mm-survey-results-item '+p+'"><span class="mm-item-number">'+i+'</span><span class="mm-item-info">'+g+' - '+p+'</span></li>');
+
+      m[i] = c;
+      ttl += m[i];
+
+    }
+
+    var results;
+    results = ( ( ttl / count ) * 100 ).toFixed(0);
+
+    jQuery('.mm-survey-results-score').html( results + '%' );
+
+  }
+
+  function goBack() {
+    jQuery('.mm-back-btn').on('click', function() {
+      jQuery('.mm-survey-bottom').slideDown();
+      jQuery('.mm-survey-results').slideUp();
+    });
+  }
