f87db851310051302191bedaafe0f4344ea65225
yuli hunsicker <yhunsicker@gmail.com>
2019-03-08 10:45:34 +0100

    work in progress

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,10 +1,30 @@
 
-document.querySelector('Login').addEventListener('click', function() {
-document.querySelector('bg-modal').style.display = 'flex';
+// Login
+
+document.getElementById('login').addEventListener('click', function(event) {
+  event.preventDefault()
+  document.querySelector('.bg-modal.login').style.display = 'flex';
 
 });
 
 
-document.quearySelector('.close').addEventListener('click', function() {
-  document.querySelector('.bg-modal').style.display = 'none';
+document.querySelector('.login .close').addEventListener('click', function(event) {
+  event.preventDefault()
+  document.querySelector('.bg-modal.login').style.display = 'none';
 });
+
+
+// Signup
+
+document.getElementById('signup').addEventListener('click', function(event) {
+  event.preventDefault()
+  document.querySelector('.bg-modal.signup').style.display = 'flex';
+
+});
+
+
+document.querySelector('.signup .close').addEventListener('click', function(event) {
+  event.preventDefault()
+  document.querySelector('.bg-modal.signup').style.display = 'none';
+});
+
