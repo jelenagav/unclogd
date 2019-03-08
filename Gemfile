cef114a3503172ac8e7a08b1e80d75643607cad0
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-08 15:58:17 +0100

    fix all the things

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'rails', '5.2.2'
 gem 'redis'
 gem 'money-rails'
 gem 'faker', '~> 1.6', '>= 1.6.6'
+gem 'stripe'
+
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index, :show] # /items
   resources :packages
-  
+
   get 'quiz', to: 'pages#quiz', as: :quiz
   resources :package_items
+
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
