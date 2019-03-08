8e059034e39d685ed7c3d1c6f2999a06909fc0a7
David Bordeleau <davidbordeleau@hotmail.com>
2019-03-08 17:41:39 +0100

    fix some more javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,7 @@
 import "bootstrap";
 import "./quiz";
 import "./form";
-import anime from 'lib/anime.es.js';
 
-import "navbar";
 // import { myTabClicks } from '../components/tab';
 // myTabClicks();
 
